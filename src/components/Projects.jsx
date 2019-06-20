357cf4b68222e0f1f6a936c6c00ad40f636c7df2
Robert Morrissey <rbrtmorrissey86@gmail.com>
2019-06-20 19:12:33 -0400

    portfolio redone in hooks and semantic-ui

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,24 +1,41 @@
 import React from 'react'
+import { Header, Item, Menu, Icon } from 'semantic-ui-react'
 import { projects } from '../assets/assets'
-import gitIcon from '../assets/github-icon.svg'
 
 const Projects = props => (
-  <>
-    <h2>Projects</h2>
+  <Item.Group divided style={{ marginTop: '3em' }}>
+    <Header as='h2' style={{ fontSize: '2.8em' }}>Projects</Header>
     {projects.map(project => (
-      <div className='project' key={project.name}>
-        <a href={project.link} className='project-img'>
-          <img src={project.img} className='project-img' alt={project.name}/>
-        </a>
-        <div className='project-container relative'>
-          <p className='project-desc'>{project.description}</p>
-          <a href={project.github} className='gitIcon project-icon absolute'>
-            <img src={gitIcon} className='gitIcon' alt='GitHub icon'/>
-          </a>
-        </div>
-      </div>
+      <Item key={project.name} style={{margin: '1em 0'}}>
+        <Item.Image
+          src={project.img}
+          as='a'
+          size='medium'
+          href={project.link}
+          target='_blank'
+        />
+        <Item.Content>
+          <Item.Header as='h2'>{project.name}</Item.Header>
+          <Item.Meta>Description</Item.Meta>
+          <Item.Description>{project.description}</Item.Description>
+          <Menu secondary icon>
+            <Menu.Item
+              name='github'
+              as='a'
+              href={project.github}
+              target='_blank'
+            >
+              <Icon
+                link
+                name='github'
+                size='large'
+              />
+            </Menu.Item>
+          </Menu>
+        </Item.Content>
+      </Item>
     ))}
-  </>
+  </Item.Group>
 )
 
 export default Projects
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
+import { Container, Card, Header, Image } from 'semantic-ui-react'
 import { skills } from '../assets/assets'
 
 const Skills = (props) => (
-  <>
-    <h2>Skills</h2>
-    <div className='skills-list'>
+  <Container style={{ marginTop: '3em' }}>
+    <Header as='h2' style={{ fontSize: '2.8em' }}>Skills</Header>
+    <Card.Group centered >
       {skills.map(skill=>(
-        <div className='skill' key={skill.name}>
-          <h3 className='skill-name'>{skill.name}</h3>
-          <img src={skill.img} className='skills-img' alt={skill.name}/>
-        </div>
+        <Card key={skill.name}>
+          <Card.Content>
+            <Image src={skill.img} size="tiny" alt={skill.name} floated='right'/>
+            <Card.Header as='h3'>{skill.name}</Card.Header>
+          </Card.Content>
+        </Card>
       ))}
-    </div>
-  </>
+    </Card.Group>
+  </Container>
 )
 
 export default Skills
